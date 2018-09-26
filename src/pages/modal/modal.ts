e8925526333cc7a81c7813d1803c16043597a8a0
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2018-09-26 16:14:17 -0400

    form bindings for modal page

--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -7,12 +7,19 @@ import { NavController, NavParams, ViewController } from 'ionic-angular';
 })
 export class ModalPage {
 
+  itemName: string;
+  itemDesc: string;
+  itemId: string;
+
   constructor(
     public navCtrl: NavController,
     public params: NavParams,
     public viewCtrl: ViewController
   ) {
-    console.log(params.get('item'));
+    const paramItem = params.get('item');
+    this.itemName = paramItem.name;
+    this.itemDesc = paramItem.description;
+    this.itemId = paramItem.id;
   }
 
   dismiss() {
