5075075b972152a7a83a5d8ffe2207ece8bcb219
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2018-09-26 16:17:17 -0400

    document update function added

--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
+
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Item } from '../../models/types';
 
 @Component({
   selector: 'page-modal',
@@ -11,19 +15,31 @@ export class ModalPage {
   itemDesc: string;
   itemId: string;
 
+  collection: AngularFirestoreCollection<Item>;
+
   constructor(
     public navCtrl: NavController,
     public params: NavParams,
-    public viewCtrl: ViewController
+    public viewCtrl: ViewController,
+    private afs: AngularFirestore,
   ) {
     const paramItem = params.get('item');
     this.itemName = paramItem.name;
     this.itemDesc = paramItem.description;
     this.itemId = paramItem.id;
+
+    this.collection = this.afs.collection<Item>('items');
   }
 
   dismiss() {
     this.viewCtrl.dismiss();
+  }
+
+  submitForm(f: NgForm) {
+    this.collection.doc(this.itemId).update({
+      name: f.value.name,
+      description: f.value.description,
+    });
   }
 
 }
