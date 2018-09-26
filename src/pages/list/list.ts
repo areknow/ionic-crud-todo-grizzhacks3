1612d155a3d16193808cfa80ae355fc2d2ba762b
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2018-09-26 15:54:01 -0400

    added firestore code to read from db

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,37 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { Item } from '../../models/types';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+  collection: AngularFirestoreCollection<Item>;
+  items$: Observable<Item[]>;
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private afs: AngularFirestore
+  ) {
+    this.collection = this.afs.collection<Item>('items');
 
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+    this.items$ = this.collection.snapshotChanges().map(actions => {
+      return actions.map(a => {
+        const data = a.payload.doc.data() as Item;
+        const id = a.payload.doc.id;
+        return { id, ...data };
       });
-    }
-  }
-
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
     });
   }
+
 }
