dc473cced112a2edd7a1dfe2c416f042c4b3c193
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2018-09-26 15:48:05 -0400

    adding document to db

--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 
-import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore'
+import { Item } from '../../models/types';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 
 @Component({
   selector: 'page-add',
@@ -13,13 +14,21 @@ export class AddPage {
   itemName: string;
   itemDesc: string = '';
 
-  constructor(public navCtrl: NavController) {
+  collection: AngularFirestoreCollection<Item>;
 
+  constructor(
+    public navCtrl: NavController,
+    private afs: AngularFirestore
+  ) {
+    this.collection = this.afs.collection<Item>('items');
   }
 
   submitForm(f: NgForm) {
     if (f.value.name) {
-      console.log(f.value);
+      this.collection.add({
+        name: f.value.name,
+        description: f.value.description
+      });
     } else {
       console.log('empty field')
     }
